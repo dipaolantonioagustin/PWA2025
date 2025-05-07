d6f6e45a71fbc7e8ac9169b2c6b74458800e47b1
Agustin Di Paolantonio <dipaolantonias@gmail.com>
2025-05-07 02:07:10 -0300

    Se armó el filtro de los eventos. Falta acomodar responsive de la vista del form del filtro y cerrar labels y agregar al filtrado nombre actor.

--- TpDiPaolantonioPWA/Controllers/EventosController.cs
+++ TpDiPaolantonioPWA/Controllers/EventosController.cs
@@ -11,5 +11,40 @@ namespace TpDiPaolantonioPWA.Controllers
             List<Evento> listadoEventos = evento.ListarEventos();
             return View(listadoEventos);
         }
+
+
+        [HttpPost]
+        public IActionResult Filtrar(Evento eventoBuscado, int? mes, int? anio)
+        {
+            Evento evento = new Evento();
+            List<Evento> listadoEventos = evento.ListarEventos();
+
+            if (!string.IsNullOrEmpty(eventoBuscado.name))
+
+            { listadoEventos = listadoEventos.Where(e => e.name.Contains(eventoBuscado.name, StringComparison.OrdinalIgnoreCase)).ToList(); }
+
+            if (!string.IsNullOrEmpty(eventoBuscado.tipo))
+
+            { listadoEventos = listadoEventos.Where(e => e.tipo == eventoBuscado.tipo).ToList(); }
+
+            if (!string.IsNullOrEmpty(eventoBuscado.foro))
+
+            { listadoEventos = listadoEventos.Where(e => e.foro == eventoBuscado.foro).ToList(); }
+
+
+            if(mes.HasValue && anio.HasValue)
+            
+            {   
+                DateTime fechaBusquedaInicio = new DateTime(anio.Value, mes.Value, 1);
+                DateTime fechaBusquedaFin = fechaBusquedaInicio.AddMonths(1).AddDays(-1);
+
+                listadoEventos = listadoEventos.Where(e => e.fechaInicio <= fechaBusquedaFin && e.fechaFin >= fechaBusquedaInicio).ToList();
+                
+            }
+
+            return View("Index",listadoEventos);
+
+
+        }
     }
 }
--- TpDiPaolantonioPWA/Models/Evento.cs
+++ TpDiPaolantonioPWA/Models/Evento.cs
@@ -11,9 +11,11 @@
 
         public string sala {  get; set; }   
 
-        public string pais {  get; set; }   
+        public string foro {  get; set; }   
 
-        public string portada {  get; set; }    
+        public string portada {  get; set; } 
+        
+        public string tipo { get; set; }
 
 
         public List<Evento> ListarEventos()
@@ -27,31 +29,71 @@
             e1.autor = "Kuitca";
             e1.sala = "Tercer Ojo";
             e1.description = "Malba presenta una conversación pública entre Guillermo Kuitca y Rodrigo Moura, flamante director artístico de la institución.";
-            e1.pais = "Argentina";
+            e1.foro = "Nacional";
             e1.fechaInicio = new DateTime(2025, 5, 5);
             e1.fechaFin = new DateTime(2025, 7, 10);
             e1.portada = "/img/Eventos/Kuitca.png";
+            e1.tipo = "Recital";
 
 
             listaEventos.Add(e1);
 
 
             Evento e2 = new Evento();
-            e2.id = 1;
+            e2.id = 2;
             e2.name = "Cine Portugues";
             e2.autor = "Directores Portugal";
             e2.sala = "Frida";
             e2.description = "La 12ª Semana de Cine Portugués reafirma su misión de exhibir en la Ciudad de Buenos Aires una muestra del mejor cine portugués contemporáneo.";
-            e2.pais = "Portugal";
+            e2.foro = "Internacional";
             e2.fechaInicio = new DateTime(2025, 6, 2);
             e2.fechaFin = new DateTime(2025, 10, 10);
             e2.portada = "/img/Eventos/CinePortugues.png";
-
+            e2.tipo = "Muestra";
 
             listaEventos.Add(e2);
 
+            Evento e3 = new Evento();
+            e3.id = 3;
+            e3.name = "Kuitca y Moura en Dialogos";
+            e3.autor = "Kuitca";
+            e3.sala = "Tercer Ojo";
+            e3.description = "Malba presenta una conversación pública entre Guillermo Kuitca y Rodrigo Moura, flamante director artístico de la institución.";
+            e3.foro = "Nacional";
+            e3.fechaInicio = new DateTime(2025, 5, 5);
+            e3.fechaFin = new DateTime(2025, 7, 10);
+            e3.portada = "/img/Eventos/Kuitca.png";
+            e3.tipo = "Conferencia";
 
+            listaEventos.Add(e3);
 
+            Evento e4 = new Evento();
+            e4.id = 3;
+            e4.name = "Kuitca y Moura en Dialogos";
+            e4.autor = "Kuitca";
+            e4.sala = "Tercer Ojo";
+            e4.description = "Malba presenta una conversación pública entre Guillermo Kuitca y Rodrigo Moura, flamante director artístico de la institución.";
+            e4.foro = "Nacional";
+            e4.fechaInicio = new DateTime(2025, 5, 5);
+            e4.fechaFin = new DateTime(2025, 7, 10);
+            e4.portada = "/img/Eventos/Kuitca.png";
+            e4.tipo = "Muestra";
+
+            listaEventos.Add(e4);
+
+            Evento e5 = new Evento();
+            e5.id = 3;
+            e5.name = "Kuitca y Moura en Dialogos";
+            e5.autor = "Kuitca";
+            e5.sala = "Tercer Ojo";
+            e5.description = "Malba presenta una conversación pública entre Guillermo Kuitca y Rodrigo Moura, flamante director artístico de la institución.";
+            e5.foro = "Internacional";
+            e5.fechaInicio = new DateTime(2025, 5, 5);
+            e5.fechaFin = new DateTime(2025, 7, 10);
+            e5.portada = "/img/Eventos/Kuitca.png";
+            e5.tipo = "Conferencia";
+
+            listaEventos.Add(e5);
 
 
 
