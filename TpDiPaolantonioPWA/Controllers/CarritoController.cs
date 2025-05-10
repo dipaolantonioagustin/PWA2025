275108a6ac44986cb2f56cd07ff69e2f36087a72
Agustin Di Paolantonio <dipaolantonias@gmail.com>
2025-05-10 01:18:56 -0300

    Se avanzó en las vistas de la Compra. Faltan detalles Técnicos y Estéticos.

--- TpDiPaolantonioPWA/Controllers/CarritoController.cs
+++ TpDiPaolantonioPWA/Controllers/CarritoController.cs
@@ -19,7 +19,7 @@ namespace TpDiPaolantonioPWA.Controllers
             Carrito carrito = new Carrito();
             carrito.tickets = ticketList;
 
-            return View(carrito);
+            return View("Index",carrito);
         }
 
         [HttpPost]
@@ -29,6 +29,8 @@ namespace TpDiPaolantonioPWA.Controllers
             Carrito carrito = JsonSerializer.Deserialize<Carrito>(carritoJson);
             carrito.usuario.socio = socio;
             carrito.CalcularDescuento();
+
+            ViewBag.socio = socio;
 
             return View("DetalleCompra", carrito);
 
@@ -64,14 +66,45 @@ namespace TpDiPaolantonioPWA.Controllers
             return View("DetalleCompra", carrito);
         }
 
-        // [HttpPost]
-        // public IActionResult ActualizarCalculo()
-        //{
+
+        public IActionResult ConfirmarCompra(Carrito c) {
+
+            Evento evento = new Evento();
+            List<Evento> listadoEventos = evento.ListarEventos();
+
+            List<Ticket> ticketList = new List<Ticket>();
+
+            ticketList.Add(new Ticket { evento_ticket = listadoEventos[0], cantidad = 2, Id = 1 });
+            ticketList.Add(new Ticket { evento_ticket = listadoEventos[3], cantidad = 4, Id = 2 });
+            ticketList.Add(new Ticket { evento_ticket = listadoEventos[1], cantidad = 1, Id = 3 });
+            Carrito carrito = new Carrito();
+            carrito.tickets = ticketList;
+            carrito.CalcularGastosOperativos();
+
+            return View("ConfirmarCompra", carrito);
+        }
+
+        public IActionResult FinalizaCompra(string medioPago, string tarjetaEmpresa, int numeroTarjeta, int clave)
+        {
+            if(clave == 4455)
+            {
+                TempData["Mensaje"] = "Felicitaciones !!! Compra Aprobada";
+                TempData["Estado"] = "Exitosa";
+            }
+            else
+            {
+                TempData["Mensaje"] = "Algo salio Mal, Compra Denegada";
+                TempData["Estado"] = "Denegada";
+
+            }
 
 
-        //  return View();
+            return RedirectToAction("ResultadoCompra");
 
 
-        //        }
+        }
+
+
+        public IActionResult ResultadoCompra() { return View(); }
     }
 }
